466c24888f3b05f6e3dafe426ceb753a12b35ed3
[SW2/서울/2반] 김태훈 <kth5954@gmail.com>
2022-10-25 13:38:52 +0900

    회원 서비스 테스트

---
+++ core/src/test/java/basic/core/MemberServiceTest.java
@@ -0,0 +1,27 @@
+package basic.core;
+
+import basic.core.member.Grade;
+import basic.core.member.Member;
+import basic.core.member.MemberService;
+import basic.core.member.MemberServiceImpl;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class MemberServiceTest {
+
+
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(1L, "Member", Grade.BASIC);
+        //when
+        this.memberService.join(member);
+        Member findMember = memberService.findMember(member.getId());
+        //then
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
